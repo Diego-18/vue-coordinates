2e84104b39becd33b5946cb2bab7bb76c8812f7a
Diego-18 <ingdiegochavez18@gmail.com>
2023-07-06 01:56:51 -0500

    feat: add-endpoint-addreess

--- services/Services.js
+++ services/Services.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const APIMaps = "https://nominatim.openstreetmap.org/";
 
+const APIOSM = "https://nominatim.openstreetmap.org/reverse?format=jsonv2";
+
 export default class Services {
   getAddress(lat, lng) {
     const url = `${APIMaps}reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
@@ -11,5 +13,9 @@ export default class Services {
   getCoords(data) {
     const url = `${APIMaps}search?format=jsonv2&q=${data}`;
     return axios.get(url);
+  }
+
+  getAddress(lat, lng) {
+    return axios.get(APIOSM + "&lat=" + lat + "&lon=" + lng);
   }
 }
